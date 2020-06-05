dd3438fcc6ae88e949599d874a7cae680241b7bf
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-04 21:01:11 -0500

    Set up post router file and imported into index

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const db = require("./data/db")
+const postsRouter = require("./data/post-router")
 
 const server = express()
 const port = 4000
 
 server.use(express.json())
+server.use("/api/posts", postsRouter)
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
