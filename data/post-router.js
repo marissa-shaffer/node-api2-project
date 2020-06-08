902dc736c93b12dd9f2d5e8392ab5bcc3e5b0ce7
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-07 21:09:15 -0500

    Post to api/posts with 400/201/500

--- data/post-router.js
+++ data/post-router.js
@@ -3,6 +3,23 @@ const db = require("./db")
 
 const router = express.Router()
 
+router.post("/api/posts", (req, res) =>{
+    if(!req.body.title || !req.body.contents) {
+        return res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    }
 
+    db.update(req.body)
+    .then((post) => {
+        res.status(201).json(post)
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({
+            error: "There was an error while saving the post to the database"
+        })
+    })
+})
 
 module.exports = router
