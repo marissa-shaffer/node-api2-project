05676146c035265e30d8845ab5f9f1ab327cd935
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-09 19:19:26 -0500

    GET for /api/posts/:id with 200/400/500 status

--- data/post-router.js
+++ data/post-router.js
@@ -69,4 +69,23 @@ router.get("/api/posts", (res, req) => {
     })
 })
 
+router.get("api/posts/:id", (res, req) => {
+    db.findById(req.params.id)
+    .then((post) => {
+        if(post){
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({
+            error: "The post information could not be retrieved." 
+        })
+    })
+})
+
 module.exports = router
