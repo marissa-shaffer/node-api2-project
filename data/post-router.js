cff705b6de75a438913f1512580f26d51270ff84
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-09 19:46:00 -0500

    Put for /api/posts/:id with 400/200/500 status

--- data/post-router.js
+++ data/post-router.js
@@ -131,4 +131,28 @@ router.delete("/api/posts/:id", (req, res) => {
     })
 })
 
+router.put("/api/posts/:id", (req, res) => {
+    if(!req.body.title || !req.body.contents){
+        return res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."  
+        })
+    }
+    db.update(req.params.id, req.body)
+    .then((post) => {
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({
+            error: "The post information could not be modified."
+        })
+    })
+})
+
 module.exports = router
