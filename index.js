29ff59a2fc27e316c14a60103713b0c9365a6574
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-09 20:03:41 -0500

    edit to server.use for posts router

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = express()
 const port = 4000
 
 server.use(express.json())
-server.use("/api/posts", postsRouter)
+server.use("/", postsRouter)
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
