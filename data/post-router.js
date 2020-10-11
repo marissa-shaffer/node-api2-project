2bd9d0e7ad8d1131fb777e9ee8105dbab5fc6d63
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-10-11 15:29:54 -0500

    Endpoint for post update

--- data/post-router.js
+++ data/post-router.js
@@ -120,4 +120,29 @@ router.delete("/posts/:id", (req, res) => {
     })
 })
 
+router.put("/posts/:id", (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+		return res.status(400).json({
+			errorMessage: "Please provide title and contents for the post.",
+		})
+	}
+
+	db.update(req.params.id, req.body)
+		.then((post) => {
+			if (post) {
+				res.status(200).json(post)
+			} else {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist.",
+				})
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({
+                error: "The post information could not be modified." ,
+			})
+		})
+})
+
 module.exports = router
