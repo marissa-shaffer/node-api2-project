2135adea87dc70db9e627bb953fa9778b7207dc2
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-10-11 13:19:34 -0500

    Set up Post end point for creating posts

--- data/post-router.js
+++ data/post-router.js
@@ -1,8 +1,28 @@
+const { Router } = require("express")
 const express = require("express")
 const db = require("./db")
 
 const router = express.Router()
 
+router.post("/posts", (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+		return res.status(400).json({
+			message: "Please provide title and contents for the post.",
+		})
+	}
 
+	db.insert(req.body)
+		.then((post) => {
+			res.status(201).json(post)
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({
+				errorMessage: "There was an error while saving the post to the database",
+			})
+		})
+})
+
+router.post()
 
 module.exports = router
