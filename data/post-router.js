84060294797f49482f8be026b27329b902c77802
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-10-11 13:50:51 -0500

    Set up post end point for adding comments

--- data/post-router.js
+++ data/post-router.js
@@ -23,6 +23,28 @@ router.post("/posts", (req, res) => {
 		})
 })
 
-router.post()
+router.post("/posts/:id/comments", (req, res) => {
+   db.findById(req.params.id)
+    .then((post) => {
+        res.status(200).json(post)
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(404).json({
+            message: "The post with the specified ID does not exist."
+        })
+    })
+
+	db.insertComment(req.params.id, req.body)
+		.then((comment) => {
+			res.status(201).json(comment)
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({
+				error: "There was an error while saving the comment to the database",
+			})
+		})
+})
 
 module.exports = router
