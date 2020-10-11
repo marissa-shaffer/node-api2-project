824532adf7f887571c6bce83d4df672280021547
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-10-11 15:15:54 -0500

    Added Delete endpoint to remove post by id

--- data/post-router.js
+++ data/post-router.js
@@ -99,4 +99,25 @@ router.get("/posts/:id/comments", (req, res) => {
         })
 }) 
 
+router.delete("/posts/:id", (req, res) => {
+    db.remove(req.params.id)
+    .then((count) => {
+        if (count > 0) {
+            res.status(200).json({
+                message: "The post has been deleted",
+            })
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist.",
+            })
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+        res.status(500).json({
+            message: "The post could not be removed",
+        }) 
+    })
+})
+
 module.exports = router
